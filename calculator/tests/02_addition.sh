325d4a05a429576c13416ae53a267528127a574c
Jacob Horne <jdhorne@stanford.edu>
2023-02-22 18:09:22 -0800

    Update test suites for addition and subtraction

--- calculator/tests/02_addition.sh
+++ calculator/tests/02_addition.sh
@@ -3,15 +3,15 @@
 # (The absolute path to the program is provided as the first and only argument.)
 CALCULATOR=$1
 
-# Test 01: Test whole number addition
+# Test 01: Test positive number addition
 if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then  # If the output of the program is not 2...
   echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
   exit 1
 fi
 
-# Test 02: Test decimal number addition
-if [[ $($CALCULATOR 0.5 + 0.25) -ne 0.75 ]]; then  # If the output of the program is not 0.75...
-  echo 'ERROR! A valid run of the calculator (0.5 + 0.25) failed to produce 0.75 as an output!'
+# Test 02: Test negative number addition
+if [[ $($CALCULATOR 2 + -1) -ne 1 ]]; then  # If the output of the program is not 0.75...
+  echo 'ERROR! A valid run of the calculator (2 + -1) failed to produce 1 as an output!'
   exit 1
 fi
 
--- calculator/tests/03_subtraction.sh
+++ calculator/tests/03_subtraction.sh
@@ -3,15 +3,15 @@
 # (The absolute path to the program is provided as the first and only argument.)
 CALCULATOR=$1
 
-# Test 01: Test whole number subtraction
+# Test 01: Test positive number subtraction
 if [[ $($CALCULATOR 1 - 1) -ne 0 ]]; then  # If the output of the program is not 0...
   echo 'ERROR! A valid run of the calculator (1 - 1) failed to produce 0 as an output!'
   exit 1
 fi
 
-# Test 02: Test decimal number subtraction
-if [[ $($CALCULATOR 0.5 - 0.25) -ne 0.25 ]]; then  # If the output of the program is not 0.25...
-  echo 'ERROR! A valid run of the calculator (0.5 - 0.25) failed to produce 0.25 as an output!'
+# Test 03: Test negative number subtraction
+if [[ $($CALCULATOR 1 - -2) -ne 3 ]]; then  # If the output of the program is not 0.25...
+  echo 'ERROR! A valid run of the calculator (1 - -2) failed to produce 3 as an output!'
   exit 1
 fi
 
